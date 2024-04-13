8e6e345ab8e813ec86101b68bf0276160ffcfe4b
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-04-13 07:43:08 -0300

    feat(home): Implement initial layout with main components.
    Add the home page layout with the following elements:
    - A greeting header with a welcome message.
    - An image with alt text 'Desenvolvedor Full Stack trabalhando em um projeto com seu desktop.'.         - A 'Conheça meus serviços' button that navigates to the services section.                              - Apply a gradient style to the services button for visual enhancement."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="fs-4">Olá! Me chamo Rafael.</h1>
-      <h2>Sou Desenvolvedor Web Full Stack</h2>
+      <div className="container text-center">
+        <h1 className="greetings fs-5 text-center px-4">
+          Olá! Sou Desenvolvedor Web Full Stack
+        </h1>
+        <p className="fs-6 text-start mx-3">
+          Com expertise em tecnologias de ponta e uma abordagem personalizada,
+          estou aqui para elevar sua presença online e trazer resultados
+          tangíveis para o seu negócio. Vamos construir algo incrível juntos.
+        </p>
+        <div className="image-container my-4">
+          <Image
+            src="/desenvolvedor-fullstack-trabalhando-no-desktop.jpg"
+            alt="Desenvolvedor Full Stack trabalhando em um projeto com seu desktop."
+            width={250}
+            height={250}
+            layout="fixed"
+            style={{
+              borderRadius: "39px",
+              boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+            }}
+          />
+        </div>
+        <div>
+          <button className="btn-custom">Conheça meus serviços</button>
+        </div>
+        <p className="fs-3 py-5">Faço suas idéias ganhar vida!</p>
+      </div>
     </main>
   );
 }
