2d337bf65cd50b71a3c14bdbe6569990655e06e3
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2024-04-17 14:30:09 -0300

    fix(footer): Adjust component alignment for improved responsiveness across different screen sizes.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="text-light py-3">
+    <footer className="text-light py-5 text-align-center">
       <div className="container">
-        <div className="row align-items-center mx-md-0 mx-1">
-          <div className="col-md-4 text-md-start mb-5">
-            <p className="mb-2 fs-5">+55 (62) 99213-6842</p>{" "}
+        <div className="row ">
+          <div className="col-12 col-md-4 mb-3 mb-md-0 text-md-start">
+            <p className="mb-2 fs-5">+55 (62) 99213-6842</p>
             <p className="fs-5">rafaelopes.dev@gmail.com</p>
           </div>
-          <div className="text-md-start mb-3 mb-md-0">
+          <div className="col-12 col-md-8 text-md-end text-sm-start">
             <p className="mb-2 fs-5">
               <a href="#" className="text-decoration-none text-light">
                 Termos de Serviços
@@ -20,23 +20,27 @@ export default function Footer() {
             </p>
           </div>
         </div>
-        <div className="col-md-12 text-md-center text-center mx-0 ms-0 mt-md-5 mt-5">
-          <p className="mb-0">&#169;2024 Desenvolvido por rafaellopes.dev</p>
+        <div className="row">
+          <div className="col-12 text-center my-2 mt-5">
+            <p className="mb-0">&#169;2024 Desenvolvido por rafaellopes.dev</p>
+          </div>
         </div>
-        <div className="col-md-7 ms-3 text-center text-md-end mt-2 mb-0">
-          <p>
-            <a href="#" className="text-decoration-none me-2">
-              GitHub
-            </a>{" "}
-            .{" "}
-            <a href="#" className="text-decoration-none mx-2">
-              LinkedIn
-            </a>{" "}
-            .{" "}
-            <a href="#" className="text-decoration-none ms-2">
-              Instagram
-            </a>
-          </p>
+        <div className="row">
+          <div className="col-12 text-center">
+            <p>
+              <a href="#" className="text-decoration-none me-2">
+                GitHub
+              </a>
+              .
+              <a href="#" className="text-decoration-none mx-2">
+                LinkedIn
+              </a>
+              .
+              <a href="#" className="text-decoration-none ms-2">
+                Instagram
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
