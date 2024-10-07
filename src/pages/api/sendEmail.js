853361b72d4072995fc50628ecfe75d5a7de4823
rafael <rafaellopes.dev@gmail.com>
2024-10-07 06:37:25 -0300

    feat(contact): Add nodemailer for contact form

---
+++ src/pages/api/sendEmail.js
@@ -0,0 +1,33 @@
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { fullName, phone, email, subject, message } = req.body;
+
+    // Configuração do transporte do Nodemailer
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS,
+      }
+    });
+
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: process.env.GMAIL_USER,
+      subject: subject,
+      text: `Nome: ${fullName}\nTelefone: ${phone}\nEmail: ${email}\nMensagem: ${message}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email enviado com sucesso!' });
+    } catch (error) {
+      res.status(500).json({ error: 'Erro ao enviar o email.' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Método ${req.method} não permitido`);
+  }
+}
